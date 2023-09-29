513ca07ea66183b4e7929ea7d9a2173930523aad
Crpto999 <81425196+Crpto999@users.noreply.github.com>
2023-09-29 21:05:33 +0800

    Update ikuuuu_account.py

--- ikuuuu_account.py
+++ ikuuuu_account.py
@@ -10,28 +10,29 @@ def main():
         print("------------正在执行第" + str(r) + "个账号----------------")
         email = i.split('&')[0]
         passwd = i.split('&')[1]
-        sign_in(email, passwd)
+        result = sign_in(email, passwd)
+        send_to_telegram(result)
         r += 1
 
 def sign_in(email, passwd):
     try:
-        body = {"email": email, "passwd": passwd}
-        headers = {
-            'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'
-        }
+        body = {"email" : email,"passwd" : passwd,}
+        headers = {'user-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'}
         resp = requests.session()
         resp.post(f'https://ikuuu.art/auth/login', headers=headers, data=body)
         ss = resp.post(f'https://ikuuu.art/user/checkin').json()
         if 'msg' in ss:
-            print(ss['msg'])
-    except:
-        print('请检查帐号配置是否错误')
+            return ss['msg']
+        else:
+            return '未知错误'
+    except Exception as e:
+        return f'请检查帐号配置是否错误：{str(e)}'
 
 def ql_env():
     if "IKUUU_ACCOUNTS" in os.environ:
-        accounts = os.environ['IKUUU_ACCOUNTS'].split('#')
-        if len(accounts) > 0:
-            return accounts
+        token_list = os.environ['IKUUU_ACCOUNTS'].split('#')
+        if len(token_list) > 0:
+            return token_list
         else:
             print("IKUUU_ACCOUNTS变量未启用")
             sys.exit(1)
@@ -39,6 +40,22 @@ def ql_env():
         print("未添加IKUUU_ACCOUNTS变量")
         sys.exit(0)
 
+def send_to_telegram(message):
+    if "TELEGRAM_BOT_TOKEN" in os.environ and "TELEGRAM_CHAT_ID" in os.environ:
+        bot_token = os.environ["TELEGRAM_BOT_TOKEN"]
+        chat_id = os.environ["TELEGRAM_CHAT_ID"]
+        url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+        data = {
+            "chat_id": chat_id,
+            "text": email+":"+message,
+        }
+        response = requests.post(url, json=data)
+        if response.status_code == 200:
+            print("Telegram 消息发送成功")
+        else:
+            print("Telegram 消息发送失败")
+    else:
+        print("未配置 TELEGRAM_BOT_TOKEN 和 TELEGRAM_CHAT_ID")
 
 if __name__ == '__main__':
     main()
