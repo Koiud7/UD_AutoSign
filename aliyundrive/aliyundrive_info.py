e066139ab8d0a6a3a58fa7eb1fb0b3d83bf3aef5
Crpto999 <81425196+Crpto999@users.noreply.github.com>
2023-10-01 22:43:12 +0800

    Update aliyundrive_info.py

--- aliyundrive/aliyundrive_info.py
+++ aliyundrive/aliyundrive_info.py
@@ -17,7 +17,12 @@ class AliyundriveInfo:
     def __str__(self) -> str:
         message_all = ''
         if self.success:
-            message_all = f'用户： {self.user_name}\n签到： 本月已签到{self.signin_count}次\n奖励： {self.reward_notice}\n任务： {self.task_notice}'
+            message_all = 
+                           '                                 \n'\
+                          f'用户：{self.user_name}\n' \
+                          f'签到：本月已签到{self.signin_count}次\n' \
+                          f'奖励：{self.reward_notice}\n' \
+                          f'任务：{self.task_notice}'
         else:
             message_all = f'签到失败\n错误信息：{self.message}'
 
