dea05c0656c1b8a598d8ea21e54a6334c51381cb
Crpto999 <81425196+Crpto999@users.noreply.github.com>
2023-10-01 22:01:25 +0800

    Update main.py

--- baidutieba/main.py
+++ baidutieba/main.py
@@ -177,11 +177,15 @@ def send_to_telegram(sign_list):
 
         length = len(sign_list)
         beijing_time:str = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time() + 28800))
-        title = "🥳百度贴吧自动签到完成" + '\n' + '\n' +"时间:  "+beijing_time + '\n' +'—————————————————'+ '\n' + "用户:  melolohappy"+ '\n'+"任务:  "+ str(length) + "个贴吧已签到" + '\n'
+        title = "**🥳百度贴吧自动签到完成**"
+
+        # 使用三个反引号包裹其他内容，并指定语言为Markdown
+        code_block = f"```\n时间:  {beijing_time}\n—————————————————\n用户:  melolohappy\n任务:  {length} 个贴吧已签到\n```"
 
         data = {
             "chat_id": chat_id,
-            "text": f" {title}",
+            "text": f"{title}\n{code_block}",
+            "parse_mode": "Markdown",
         }
         response = requests.post(url, json=data)
         if response.status_code == 200:
